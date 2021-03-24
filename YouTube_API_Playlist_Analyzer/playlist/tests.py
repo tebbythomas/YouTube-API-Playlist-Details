24c09698f341084f7160cf0066011f974b3aa977
Tebby Thomas <tebby.thomas@gmail.com>
2021-03-24 16:10:01 +0530

    Adding test cases

--- YouTube_API_Playlist_Analyzer/playlist/tests.py
+++ YouTube_API_Playlist_Analyzer/playlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import RequestsClient
+import json
 
-# Create your tests here.
+
+client = RequestsClient()
+response = client.get('http://127.0.0.1:8000/playlist/PLrtCHHeadkHptUb0gduz9pxLgvtKWznKj')
+assert response.status_code == 200
+reponse_body = json.loads(response._content)
+assert reponse_body['video_count'] == 7
+
+response = client.get('http://127.0.0.1:8000/playlist/abc')
+assert response.status_code == 500
+reponse_body = json.loads(response._content)
+assert reponse_body['detail'] == 'Invalid playlist id entered'
+
